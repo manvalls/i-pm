4b43db1c04e6263649ccc74e4c4f005ad2fc9882
Manuel Valls Fernández <manolo@vlrz.es>
2015-04-28 20:03:21 +0200

    Now using i-peer/from/ws

--- ws.js
+++ ws.js
@@ -1,8 +1,7 @@
 var ws = require('ws'),
     Emitter = require('y-emitter'),
-    
-    on = require('u-proto/on'),
-    once = require('u-proto/once');
+    from = require('i-peer/from/ws'),
+    on = require('u-proto/on');
 
 module.exports = function getPeerMachine(server,path){
   var emitter = new Emitter(),
@@ -19,45 +18,6 @@ module.exports = function getPeerMachine(server,path){
 }
 
 function onConnection(e,en,emitter){
-  var ws = e[0],
-      inP,outP;
-  
-  inP = new Emitter.Hybrid();
-  outP = new Emitter.Hybrid();
-  Emitter.chain(inP,outP);
-  
-  inP.on('msg',sendMsg,ws);
-  inP.once('closed',close,ws);
-  
-  ws[on]('message',onMsg,inP);
-  ws[once]('close',onceClose,inP);
-  
-  inP.set('ready');
-  emitter.give('peer',outP);
-}
-
-function sendMsg(msg,en,ws){
-  try{ ws.send(JSON.stringify(msg)); }
-  catch(e){ }
-}
-
-function close(e,en,ws){
-  ws.close();
-  inP.unset('ready');
-  inP.set('closed');
-}
-
-function onMsg(e,en,inP){
-  var msg = e[0];
-  
-  try{ msg = JSON.parse(msg); }
-  catch(e){ return; }
-  
-  inP.give('msg',msg);
-}
-
-function onceClose(e,en,inP){
-  inP.unset('ready');
-  inP.set('closed');
+  emitter.give('peer',from(e[0]));
 }
 
